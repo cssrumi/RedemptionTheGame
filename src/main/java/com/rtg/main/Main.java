3ddc0141c2a5ea9f6664378abc42ef1ad8a0f6d7
Adam Rumiński <Adam.Ruminski@atena.pl>
2018-11-28 14:43:24 +0100

    f

---
+++ src/main/java/com/rtg/main/Main.java
@@ -0,0 +1,16 @@
+package com.rtg.main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Redemption The Game");
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+        window.setSize(gamePanel.getSize());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setVisible(true);
+    }
+}
